22788d7026890bc966285ae9e43aa0f2b8f72b3e
Richard Stanway <r1ch@r1ch.net>
2017-06-22 14:55:16 +0200

    Initial commit

---
+++ check_for_64bit_visual_studio_2013_runtimes.c
@@ -0,0 +1,28 @@
+/*
+    Copyright (C) 2017 Richard Stanway
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <windows.h>
+
+int WINAPI _start (void)
+{
+	SetErrorMode (SEM_FAILCRITICALERRORS);
+	if (!LoadLibraryW (L"MSVCR120.DLL"))
+		return 126;
+	if (!LoadLibraryW (L"MSVCP120.DLL"))
+		return 126;
+	return 0;
+}
